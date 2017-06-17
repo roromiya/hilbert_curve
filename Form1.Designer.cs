9d428e1465835426b4a2ad004de3057a691558d3
roromiya <dbtkdfhr2000@naver.com>
2017-06-17 23:16:30 +0900

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace howto_hilbert_curve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtDepth = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picCanvas.Location = new System.Drawing.Point(94, 0);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(633, 478);
+            this.picCanvas.TabIndex = 9;
+            this.picCanvas.TabStop = false;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(23, 37);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(47, 22);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "Draw";
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtDepth
+            // 
+            this.txtDepth.Location = new System.Drawing.Point(51, 7);
+            this.txtDepth.Name = "txtDepth";
+            this.txtDepth.Size = new System.Drawing.Size(37, 21);
+            this.txtDepth.TabIndex = 6;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(5, 7);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(37, 12);
+            this.Label1.TabIndex = 5;
+            this.Label1.Text = "Depth";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 478);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtDepth);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "howto_hilbert_curve";
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox picCanvas;
+        internal System.Windows.Forms.Button btnGo;
+        internal System.Windows.Forms.TextBox txtDepth;
+        internal System.Windows.Forms.Label Label1;
+    }
+}
+
